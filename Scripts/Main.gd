6ea7f3dfa472ee5f4b25826c7cf3b11c78abf6c0
ZatzuStudent <20192681@s.ubaguio.edu>
2024-03-21 01:45:21 +0800

    trying to fix
    trying to fix

--- Scripts/Main.gd
+++ Scripts/Main.gd
@@ -5,8 +5,8 @@ func _ready():
 	
 func _process(delta):
 	# Simple camera pan lang try
-	if $Player && $Player.velocity != Vector2.ZERO:
-		$Camera2D.position += ($Player.position - $Camera2D.position)/3 * delta
+	#if $Player && $Player.velocity != Vector2.ZERO:
+	#	$Camera2D.position += ($Player.position - $Camera2D.position)/3 * delta
 
 	if $CanvasLayer/CheckButton.button_pressed:
 		$"Enemy Type 1".change_art()
