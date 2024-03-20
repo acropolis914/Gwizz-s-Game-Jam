4f587d6a74ab656cd6d228d926b411c5367d8ebb
PaulHarvey <hed-phsuarnaba@smu.edu.ph>
2024-03-20 23:03:56 +0800

    added shit

--- Scripts/Main.gd
+++ Scripts/Main.gd
@@ -1,1 +1,4 @@
 extends Node2D
+
+func _process(delta):
+	$Camera2D.position += ($Player.position - $Camera2D.position)/12 * delta
--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -9,7 +9,10 @@ func _ready():
 func for_movement():
 	var input_direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
 	velocity = input_direction * speed
-
+	
+	#if velocity.x < 0:
+		#$AnimatedSprite2D.flipvs
+		
 func for_dash_and_kill():
 	if Input.is_action_just_pressed("dash"):
 		speed = 1400
@@ -22,3 +25,7 @@ func _physics_process(_delta):
 	for_movement()
 	for_dash_and_kill()
 	move_and_slide()
+	
+func _process(delta):
+	$AnimatedSprite2D.animation = "walk"
+	$AnimatedSprite2D.play()
